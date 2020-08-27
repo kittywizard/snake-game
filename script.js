da2d965ab98acd7bb5c33698b12ab3e56aa3d4ac
marleigh <volcana@gmail.com>
2020-08-27 13:04:25 -0700

    adding comments to js for grid creation

---
+++ script.js
@@ -0,0 +1,13 @@
+const grid = document.querySelector('.grid');
+const score = document.getElementById('score');
+const button = document.getElementById('start');
+
+function createGrid() {
+    //create elements to put them into the grid div
+
+    //we need 100 of these elements
+
+    //add styling to the elements
+
+    //save the squares into an array
+}
