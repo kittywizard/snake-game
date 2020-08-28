6c30f995a0b1f6b54a14ff91427d449acc73e571
marleigh <volcana@gmail.com>
2020-08-28 10:47:35 -0700

    a wild snake has appeared!

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const grid = document.querySelector('.grid');
 const score = document.getElementById('score');
 const button = document.getElementById('start');
 let squareArr = [];
+let currentSnake = [0, 1, 2];
 
 function createGrid() {
 
     const squareSize = 100;
 
-    for(var i = 0; i <= squareSize - 1; i++){
-        let newDiv = document.createElement('div');
-        newDiv.className = 'square';
-        squareArr.push(newDiv);
-        //grid.appendChild(newDiv);
-        //console.log(newDiv);
+    for(let i = 0; i <= (squareSize - 1); i++){
+        let square = document.createElement('div');
+        square.classList.add('square');
+            //use classList.add instead of className as it allows for multiple class assignments
+            //and has other functions you can use
+        squareArr.push(square);
+        grid.appendChild(squareArr[i]);
     }
  
 }
 
 createGrid();
-console.log(squareArr);
+
+//index is whatever position the array is at currently
+//=> arrow function to add each of the snake's positions into the grid
+currentSnake.forEach(index => squareArr[index].classList.add('snake'));
