7e6b412c74a5922078a07a97dc94648ed4141bec
marleigh <volcana@gmail.com>
2020-08-28 15:13:11 -0700

    basic snake movement

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const grid = document.querySelector('.grid');
 const score = document.getElementById('score');
 const button = document.getElementById('start');
 let squareArr = [];
-let currentSnake = [0, 1, 2];
+let currentSnake = [2, 1, 0];
+let direction = 1;
 
 function createGrid() {
 
@@ -24,3 +25,19 @@ createGrid();
 //index is whatever position the array is at currently
 //=> arrow function to add each of the snake's positions into the grid
 currentSnake.forEach(index => squareArr[index].classList.add('snake'));
+
+function move() {
+
+    //pop the tail off the snake and store it
+    const tail = currentSnake.pop();
+    
+    //removing the styling by finding the tail position in the original array and removing the style
+    squareArr[tail].classList.remove('snake');
+
+    //adding a new element in the direction (just right for now)
+    currentSnake.unshift(currentSnake[0] + direction);
+    //add the styling to the current snake (in the grid array as well) 
+    squareArr[currentSnake[0]].classList.add('snake');
+}
+
+move();
